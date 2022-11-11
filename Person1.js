2c65ba8a995e7746e39525cde235d454f7ae9694
Aradhya kumar mishra <116092894+Aradhyadeveloper@users.noreply.github.com>
2022-11-11 21:05:41 +0530

    Add files via upload

---
+++ Person1.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// class Person1 extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.props = props
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>
+                    
+//                     gautam {this.props.name} ji {this.props.age}
+//                 </h1>
+//             </div>
+//         )
+//     }
+// }
+// export default Person1;
