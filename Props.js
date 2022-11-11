b27f7b65f9ba0e64d9b940af0fe50fe2c3e27345
Aradhya kumar mishra <116092894+Aradhyadeveloper@users.noreply.github.com>
2022-11-11 21:06:05 +0530

    Add files via upload

---
+++ Props.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import Person from './Person'
+
+// function Props(){
+//     return(
+//     <div>
+//         <Person name ='aradhay' age ='26'/>
+//         <Person name='vineet' age='25' />
+//         <Person name='bhagwat' age='24' />
+
+//         <Person name='ankit' age='23' />
+
+
+
+//     </div>
+//     )
+// } 
+
+// export default Props;
